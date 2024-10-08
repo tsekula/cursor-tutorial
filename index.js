9fc84c86d73dceefc4af0f6ccb45b2e02f50680b
Tom Sekula <tom@sekula.com>
2024-10-08 14:15:35 +0200

    nothing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('you are the greatest\n');
+    res.end('you are the greatest!\n');
 });
 
 const PORT = 3000;
